532dbcc167ba54e6a27581d9d4d2fe95b09e5082
gruby.Wang <gruby.Wang@shijigroup.com>
2018-03-29 07:49:06 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component ,Fragment} from 'react'
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
-
+// BrowserRouter H5模式  HashRouter Hash模式
 class A extends Component {
   render () {
     return (
@@ -9,22 +9,36 @@ class A extends Component {
   }
 }
 
-class B extends Component {
-  render () {
-    return (
-      <div>组件B</div>
-    )
-  }
+// class B extends Component {
+//   render () {
+//     return (
+//       <div>组件B</div>
+//     )
+//   }
+// }
+
+// 无状态组件写法
+// const B =(props)=>{
+//   console.log(props)
+//   return(<div>组件B</div>)
+// }
+//  下面这种写法{match}类似于从props取出match
+const B =({match})=>{
+  console.log(match)
+  return(<div>组件B</div>)
 }
 
 class C extends Component {
-
+  constructor (props){
+    super(props)
+    console.log(this.props)
+  }
   render()
   {
     return (
       <div>
         <Switch>
-          <Route exact path={`${this.props.match.path}`}
+          <Route exact path={`${this.props.match.path}`}  // exact 严格匹配路径
                  render={() => {
                    return (<div>不带参的组件C</div>)
                  }}/>
@@ -41,21 +55,29 @@ class C extends Component {
 
 
 class App extends Component {
+  handleRouter(){
+    console.log(this.props)
+  }
   render () {
     return (<Router>
-        <div>
+        {/* Fragment 片段 完全是为了减少不合理的标签嵌套及减少DOM节点*/}
+        <Fragment>
+          {/*链接导航*/}
           <ul>
             <li><Link to='/a'>A组件</Link></li>
             <li><Link to='/b'>B组件</Link></li>
             <li><Link to={'/c'}>C组件</Link></li>
             <li><Link to={'/c/123'}>C组件(带参数)</Link></li>
-
           </ul>
+          {/*函数式导航*/}
+          <button onClick={()=>this.handleRouter()}>GO B组件</button>
+
+          {/* 路由规则*/}
           <Route path='/a' component={A}/>
           <Route path='/b' component={B}/>
           <Route path={'/c'} component={C}/>
           <Route path={'/c/:id'} component={C}/>
-        </div>
+        </Fragment>
 
       </Router>
     )
